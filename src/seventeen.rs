4cb73b92c3696c9729d501bf9b0e86e7de99f7f6
Garry Hill <garry@magnetised.net>
2025-12-01 21:02:41 +0000

    aunty

--- src/seventeen.rs
+++ src/seventeen.rs
@@ -3,27 +3,38 @@ use crate::display::{Animate, Point, Points, Rgba, Sprite, SpriteColour, HEIGHT,
 #[rustfmt::skip]
 
 const PIXELS: [&str; 10] = [
-    "  x  xxxxx",
-    " xx      x",
-    "  x      x",
-    "  x      x",
-    "  x     x ",
-    "  x     x ",
-    "  x    x  ",
-    "  x    x  ",
-    "  x    x  ",
-    " xxx   x  ",
+    "  xx  xxxxx",
+    " xxx  xxxxx",
+    "  xx     xx",
+    "  xx     x*",
+    "  xx    *x ",
+    "  xx    xx ",
+    "  xx    x* ",
+    "  xx   *x  ",
+    " xxxx  xx  ",
+    " xxxx  xx  ",
 ];
 
-const COLOURS: [SpriteColour; 1] = [(
-    "x",
-    Rgba {
-        r: 0.0,
-        g: 1.0,
-        b: 0.0,
-        a: 1.0,
-    },
-)];
+const COLOURS: [SpriteColour; 2] = [
+    (
+        "x",
+        Rgba {
+            r: 0.0,
+            g: 1.0,
+            b: 0.0,
+            a: 1.0,
+        },
+    ),
+    (
+        "*",
+        Rgba {
+            r: 0.0,
+            g: 0.7,
+            b: 0.0,
+            a: 1.0,
+        },
+    ),
+];
 
 pub struct Seventeen {
     points: Points,
